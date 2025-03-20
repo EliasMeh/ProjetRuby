b38a5e1cfa6eabf9a795a2bdffe17c96a25d053d
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2025-03-20 13:49:00 +0100

    Correction des test unitaires

--- test/controllers/authors_controller_test.rb
+++ test/controllers/authors_controller_test.rb
@@ -1,23 +1,34 @@
 require "test_helper"
 
 class AuthorsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @author = authors(:one)
+  end
+
   test "should get index" do
-    get authors_index_url
+    get authors_url
     assert_response :success
   end
 
   test "should get show" do
-    get authors_show_url
+    get author_url(@author)
     assert_response :success
   end
 
   test "should get new" do
-    get authors_new_url
+    get new_author_url
     assert_response :success
   end
 
   test "should get edit" do
-    get authors_edit_url
+    get edit_author_url(@author)
     assert_response :success
+  end
+
+  test "should create author" do
+    assert_difference("Author.count") do
+      post authors_url, params: { author: { name: "New Author" } }
+    end
+    assert_redirected_to author_url(Author.last)
   end
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,23 +1,50 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @book = books(:one)
+    @author = authors(:one)
+  end
+
   test "should get index" do
-    get books_index_url
+    get books_url
     assert_response :success
   end
 
   test "should get show" do
-    get books_show_url
+    get book_url(@book)
     assert_response :success
   end
 
   test "should get new" do
-    get books_new_url
+    get new_book_url
     assert_response :success
   end
 
   test "should get edit" do
-    get books_edit_url
+    get edit_book_url(@book)
     assert_response :success
+  end
+
+  test "should create book" do
+    assert_difference("Book.count") do
+      post books_url, params: { book: { title: "New Book", author_id: @author.id, publication_date: Date.today } }
+    end
+    assert_redirected_to book_url(Book.last)
+  end
+
+  test "should mark book as taken" do
+    patch mark_as_taken_book_url(@book)
+    assert_redirected_to book_url(@book)
+    @book.reload
+    assert_equal "taken", @book.status
+  end
+
+  test "should mark book as returned" do
+    @book.update(status: "taken")
+    patch mark_as_returned_book_url(@book)
+    assert_redirected_to book_url(@book)
+    @book.reload
+    assert_equal "available", @book.status
   end
 end
