3825748fc43ceb67dc720311324b46cdff0736e7
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2025-03-20 11:34:03 +0100

    Fonctionnement pages+ boutons

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def index
-    @authors = Author.all
+    @authors = Author.includes(:books)
   end
 
   def show
@@ -11,37 +11,7 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
-  def create
-    @author = Author.new(author_params)
-    if @author.save
-      redirect_to @author
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
   def edit
     @author = Author.find(params[:id])
-  end
-
-  def update
-    @author = Author.find(params[:id])
-    if @author.update(author_params)
-      redirect_to @author
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @author = Author.find(params[:id])
-    @author.destroy
-    redirect_to authors_path, status: :see_other
-  end
-
-  private
-
-  def author_params
-    params.require(:author).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-
 Rails.application.routes.draw do
-  root "home#index"
+  root "home#index"   # This sets the home page as the landing page
+
+  # Routes for authors
   resources :authors
+
+  # Routes for books
   resources :books
+
+  # You can add additional routes if needed
 end
